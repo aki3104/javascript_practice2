74c2ec091f2706cb90b65216010f1df26302a178
Satoshi <aki319809@gmail.com>
2019-11-12 20:01:27 +0900

    add_table

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 'usre strict';
 
 // 要件定義
-let Task = document.getElementById('task');
+const Task = document.getElementById('task');
 const action = document.getElementById('action');
 const result = document.getElementById('result');
-let list = [];
+const tasks = [];
 
 // クリックイベント
 action.addEventListener('click',()=>{
-  const div = document.createElement('div');
-  result.appendChild(div);
-  const p = document.createElement('p');
-  div.appendChild(p);
-  list.push(Task.value);
-  p.textContent = `${list.length-1}` + "    " + `${list[list.length-1]}`;
+  tasks.push(Task.value);
   Task.value = "";
 
+  const tr = document.createElement('tr');
+  result.appendChild(tr);
+
+  const td1 = document.createElement('td');
+  td1.textContent =  tasks.length-1;
+  tr.appendChild(td1);
+
+  const td2 = document.createElement('td');
+  td2.textContent = tasks[tasks.length-1];
+  tr.appendChild(td2);
+
   //ボタンの作成
-  const BtnID = document.createElement('button');
-  div.appendChild(BtnID);
-  BtnID.textContent = '作業中'
+  const td3 = document.createElement('td');
+  td3.textContent = '作業中'
+  td3.setAttribute = ('class', 'btn');
+  tr.appendChild(td3);
 
   const BtnDel = document.createElement('button');
-  div.appendChild(BtnDel);
+  tr.appendChild(BtnDel);
   BtnDel.textContent = '削除'
-  console.log(list);
+  console.log(tasks);
   })
 
