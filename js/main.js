2a2bfa54c0efde1ba3013ef541ecc9299f52ca4a
Satoshi <aki319809@gmail.com>
2019-11-16 18:38:28 +0900

    delete,radio

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const action = document.getElementById('action');
 const result = document.getElementById('result');
 const tasks = [];
 
+
 // クリックイベント
 action.addEventListener('click',()=>{
   tasks.push(Task.value);
@@ -22,14 +23,57 @@ action.addEventListener('click',()=>{
   td2.textContent = tasks[tasks.length-1];
   tr.appendChild(td2);
 
+  //ステータスボタン作成、クリック動作
   const td3 = document.createElement('td');
   td3.textContent = '作業中'
   td3.className = 'btn';
   tr.appendChild(td3);
+  td3.parentNode.className = 'trWrk';
+  td3.addEventListener('click',()=>{
+    if (td3.textContent === '作業中'){
+      td3.textContent = '完了'
+      td3.parentNode.className = 'trComp'
+    }else{
+      td3.textContent = '作業中'
+      td3.parentNode.className = 'trWrk'
+    }
+  })
 
-  const BtnDel = document.createElement('button');
-  tr.appendChild(BtnDel);
-  BtnDel.textContent = '削除'
+  //削除ボタン作成、クリック動作
+  const btnDel = document.createElement('button');
+  tr.appendChild(btnDel);
+  btnDel.textContent = '削除'
+  btnDel.addEventListener('click',()=>{
+    btnDel.parentNode.textContent = ""
+  })
 
 })
 
+// ラジオボタン選択
+const radioselect = ()=>{
+  const work = document.querySelectorAll('input[type="radio"]')[1].checked;
+  const comp = document.querySelectorAll('input[type="radio"]')[2].checked;
+
+  //表示の初期化
+  document.querySelectorAll('tr').forEach(trAll => {
+    trAll.style.display = ''
+  })
+
+    if(work === true){
+      //完了を非表示
+        document.querySelectorAll('tr.trComp').forEach(trComp=>{
+          trComp.style.display = 'none';
+        })
+    }else if(comp === true){
+      //作業中を非表示
+      document.querySelectorAll('tr.trWrk').forEach(trWrk=>{
+        trWrk.style.display = 'none';
+      })
+    }else{
+      //非表示を全て表示に
+      document.querySelectorAll('tr').forEach(trAll => {
+        trAll.style.display = ''
+      })
+
+    }
+}
