d757635e2b05468e1089fb31deef14c6bb4581a2
Satoshi <aki319809@gmail.com>
2019-11-22 19:34:19 +0900

    todoObject

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'usre strict';
 
 // 要件定義
-const task = document.getElementById('task');
+const inputTask = document.getElementById('task');
 const action = document.getElementById('action');
 const result = document.getElementById('result');
-const tasks = [];
+const todos = [];
 
 //削除ボタンの生成
 const createDeleteBtn = () => {
@@ -36,47 +36,156 @@ const createStatusBtn = (trElem)　=>　{
 
 // クリックイベント
 action.addEventListener('click',() => {
+  const todo = {
+    index: todos.length,
+    value: inputTask.value, 
+    status: '作業中'
+  };
 
-  tasks.push(task.value);
-  task.value = "";
+  todos.push(todo);
+  inputTask.value = "";
 
-  const tr = document.createElement('tr');
-  result.appendChild(tr);
+ 
+//   // todo表示ようのtr要素を生成
+//     const taskElement = document.createElement('tr');
 
-  const idTdIndex = document.createElement('td');
-  idTdIndex.textContent =  tasks.length - 1;
-  tr.appendChild(idTdIndex);
+//     //ID表示ようのtd要素を生成
+//     const idElement = document.createElement('td');
+//     const taskId = filteredTodos.id;
+//     idElement.textContent = taskId;
 
-  const idTdTask = document.createElement('td');
-  idTdTask.textContent = tasks[tasks.length - 1];
-  tr.appendChild(idTdTask);
+//     //task表示用td要素を生成
+//     const commentElement = document.createElement('td');
+//     commentElement.textContent = filteredTodos.task;
 
-  //ステータスボタンクリック動作
-  createStatusBtn(tr);
+//     //statusBtn表示用td要素を生成
+//     const statusBtnElement = document.createElement('td');
+//     const statusBtn = createStatusBtn(taskId - 1);
+//     statusBtnElement.appendChild(statusBtn);
 
-  //削除ボタンクリック動作
-  const delBtn = createDeleteBtn();
-  tr.appendChild(delBtn);
-  delBtn.addEventListener('click',() => {
-    delBtn.parentNode.textContent = ""
-  })
-})
+//     //deleteBtn表示用td要素を生成
+//     const removeBtnElement = document.createElement('td');
+//     const removeBtn = createRrmoveBtn(taskId - 1);
+//     removeBtnElement.appendChild(removeBtn);
+
+//     //生成したtdをtrにセット
+//     taskElement.appendChild(idElement);
+//     taskElement.appendChild(commentElement);
+//     taskElement.appendChild(statusBtnElement);
+//     taskElement.appendChild(removeBtnElement);
+    
+//     todoList.appendChild(taskElement);
+//   });
+// // }
+
+//   // const tr = document.createElement('tr');
+//   // result.appendChild(tr);
+
+//   // const idTdIndex = document.createElement('td');
+//   // idTdIndex.textContent =  todos.length - 1;
+//   // tr.appendChild(idTdIndex);
+
+//   // const idTdTask = document.createElement('td');
+//   // idTdTask.textContent = todos[todos.length - 1];
+//   // tr.appendChild(idTdTask);
+
+//   // //ステータスボタンクリック動作
+//   // createStatusBtn(tr);
+
+//   // //削除ボタンクリック動作
+//   // const delBtn = createDeleteBtn();
+//   // tr.appendChild(delBtn);
+//   // delBtn.addEventListener('click',() => {
+//   //   delBtn.parentNode.textContent = ""
+//   // })
+
+// // const displyaTodos = radioType => {
+
+// //   //選択されているラジオボタンに応じてtodosをフィルタリング
+// //   const filteredTodos = Todos(radioType);
+
+// //   //tbodyタグを取得
+// //   const result = document.getElementById('result');
+
+// //   //初期化
+// //   result.textContent = '';
+
+// //   filteredTodos.forEach((filteredTodos, index ) => {
+// //     //todo表示ようのtr要素を生成
+// //     const taskElement = document.createElement('tr');
+
+// //     //ID表示ようのtd要素を生成
+// //     const idElement = document.createElement('td');
+// //     const taskId = filteredTodos.id;
+// //     idElement.textContent = taskId;
+
+// //     //task表示用td要素を生成
+// //     const commentElement = document.createElement('td');
+// //     commentElement.textContent = filteredTodos.task;
+
+// //     //statusBtn表示用td要素を生成
+// //     const statusBtnElement = document.createElement('td');
+// //     const statusBtn = createStatusBtn(taskId - 1);
+// //     statusBtnElement.appendChild(statusBtn);
+
+// //     //deleteBtn表示用td要素を生成
+// //     const removeBtnElement = document.createElement('td');
+// //     const removeBtn = createRrmoveBtn(taskId - 1);
+// //     removeBtnElement.appendChild(removeBtn);
+
+// //     //生成したtdをtrにセット
+// //     taskElement.appendChild(idElement);
+// //     taskElement.appendChild(commentElement);
+// //     taskElement.appendChild(statusBtnElement);
+// //     taskElement.appendChild(removeBtnElement);
+    
+// //     todoList.appendChild(taskElement);
+// //   });
+// // }
+
+//   // const tr = document.createElement('tr');
+//   // result.appendChild(tr);
+
+//   // const idTdIndex = document.createElement('td');
+//   // idTdIndex.textContent =  todos.length - 1;
+//   // tr.appendChild(idTdIndex);
+
+//   // const idTdTask = document.createElement('td');
+//   // idTdTask.textContent = todos[todos.length - 1];
+//   // tr.appendChild(idTdTask);
+
+//   // //ステータスボタンクリック動作
+//   // createStatusBtn(tr);
+
+//   // //削除ボタンクリック動作
+//   // const delBtn = createDeleteBtn();
+//   // tr.appendChild(delBtn);
+//   // delBtn.addEventListener('click',() => {
+//   //   delBtn.parentNode.textContent = ""
+//   // })
+// })
 
 // ラジオボタン選択
 const radioselect = () => {
   const work = document.querySelectorAll('input[type="radio"]')[1].checked;
   const comp = document.querySelectorAll('input[type="radio"]')[2].checked;
+  todos.forEach((index, value, sutatus) => {
 
-  //表示の初期化
-  document.querySelectorAll('tr').forEach(trAll => {
-    trAll.style.display = ''
-  })
+    //表示の初期化
+    document.querySelectorAll('tr').forEach(trAll => {
+      trAll.style.display = ''
+    })
 
     if(work === true){
       //完了を非表示
-        document.querySelectorAll('tr.trComp').forEach(trComp => {
-          trComp.style.display = 'none';
-        })
+      const filterTodo = {
+        index: todo.index,
+        value: todo.value, 
+        status: todo.status,
+      };
+      filteredTodos.push(filterTodo);
+      console.log(filteredTodos);
+      
     }else if(comp === true){
       //作業中を非表示
       document.querySelectorAll('tr.trWrk').forEach(trWrk => {
@@ -87,6 +196,7 @@ const radioselect = () => {
       document.querySelectorAll('tr').forEach(trAll => {
         trAll.style.display = ''
       })
-
     }
+    })
   }
+})
